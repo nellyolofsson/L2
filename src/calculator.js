2f5ef086618c3c8cb7a053a1995867b12e1bc615
Nelly <no222hh@student.lnu.se>
2023-09-12 09:55:20 +0200

    calculator class

--- src/calculator.js
+++ src/calculator.js
@@ -1,61 +1,82 @@
+/**
+ * Calculator class for performing calculations on electricityprice data.
+ */
 export class Calculator {
-    calculateAvaragePrice (data) {
-        if (!Array.isArray(data)) {
-            throw new Error('Data must be array')
-        }
-         // Flatten the array of price objects into a single array
-         const flattenedPriceData = data.flat()
+  /**
+   * Calculates the average price from the input data.
+   *
+   * @param {Array} data - An array of price data objects.
+   * @returns {number} The calculated average price.
+   */
+  calculateAvaragePrice (data) {
+    this.faultHandling(data)
+    // Flatten the array of price objects into a single array
+    // Använder flat() för att göra data till en array
+    const flattenedPriceData = data.flat()
+    // Plockar ut data som jag behöver
+    const filteredData = flattenedPriceData.filter(item => item.SEK_per_kWh !== undefined)
+    if (filteredData.length === 0) {
+      return 0
+    }
+    // filteredData är en array med objekt, och varje objekt har en egenskap SEK_per_kWh.
+    // totalSum beräknas genom att använda metoden reduce för att iterera över arrayen 
+    // filteredData.length används för att få antalet objekt i arrayen filteredData, vilket representerar det totala antalet priser.
+    // averagePrice beräknas sedan genom att dela totalSum (summan av alla priser) med filteredData.length, vilket ger dig det genomsnittliga priset.
 
-         // Filter out items with undefined SEK_per_kWh
-         const filteredData = flattenedPriceData.filter(item => item.SEK_per_kWh !== undefined)
- 
-         if (filteredData.length === 0) {
-             return 0
-         }
- 
-         const totalSum = filteredData.reduce((sum, item) => sum + item.SEK_per_kWh, 0)
-         const averagePrice = totalSum / filteredData.length
-         return averagePrice
-     }
+    const totalSum = filteredData.reduce((sum, item) => sum + item.SEK_per_kWh, 0)
+    const averagePrice = totalSum / filteredData.length
+    return averagePrice
+  }
 
-     calculateMinimumPrice (data) {
-        if (!Array.isArray(data)) {
-            throw new Error('Data must be array')
-        }
+  /**
+   * Calculates the minimum price from the input data.
+   *
+   * @param {Array} data - An array of price data objects.
+   * @returns {number} The calculated minimum price.
+   */
+  calculateMinimumPrice (data) {
+    this.faultHandling(data)
+    if (data.length === 0) {
+      return 0
+    }
+    let min = data[0].SEK_per_kWh
+    for (const item of data) {
+      if (item.SEK_per_kWh < min) {
+        min = item.SEK_per_kWh
+      }
+    }
+    return min
+  }
 
-        if (data.length === 0) {
-            return 0
-        }
+  /**
+   * Calculates the maximum price from the input data.
+   *
+   * @param {Array} data - An array of price data objects.
+   * @returns {number} The calculated maximum price.
+   */
+  calculateMaximumPrice (data) {
+    this.faultHandling(data)
+    if (data.length === 0) {
+      return 0
+    }
+    let max = data[0].SEK_per_kWh
+    for (const item of data) {
+      if (item.SEK_per_kWh > max) {
+        max = item.SEK_per_kWh
+      }
+    }
+    return max
+  }
 
-        let min = data[0].SEK_per_kWh
-        for(const item of data) {
-            if(item.SEK_per_kWh < min) {
-                min = item.SEK_per_kWh
-            }
-        }
-
-        return min
-
-      
-     }
-
-     calculateMaximumPrice (data) {
-        if (!Array.isArray(data)) {
-            throw new Error('Data must be array')
-        }
-        if (data.length === 0) {
-            return 0 // Return 0 for an empty array, or you can throw an error if preferred
-        }
-    
-        let max = data[0].SEK_per_kWh // Initialize max with the first value in the array
-    
-        for (const item of data) {
-            if (item.SEK_per_kWh > max) {
-                max = item.SEK_per_kWh // Update max if a larger value is found
-            }
-        }
-    
-        return max
-
-     }
+  /**
+   * Checks if the input is an array and throws an error if it's not.
+   *
+   * @param {Array} data - The input data to be checked.
+   * @throws {Error} Throws an error if the input is not an array.
+   */
+  faultHandling (data) {
+    if (!Array.isArray(data)) {
+      throw new Error('The argument is not an array: ' + JSON.stringify(data))
+    }
+  }
 }
