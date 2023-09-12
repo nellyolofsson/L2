9dadbb3f1e63cc59eb509773e93e68d63ca0ee2f
Nelly <no222hh@student.lnu.se>
2023-09-12 09:55:56 +0200

    filter class

--- src/filter.js
+++ src/filter.js
@@ -1,19 +1,27 @@
+/**
+ * Filter class for filtering and processing data.
+ */
 export class Filter {
-    static filterData(data) {
-        const filteredData = data.map(item => {
-            return {
-                region: item.region,
-                prices: item.data.map(entry => {
-                    return {
-                        time_start: entry.time_start,
-                        time_end: entry.time_end,
-                        SEK_per_kWh: entry.SEK_per_kWh,
-                        EUR_per_kWh: entry.EUR_per_kWh,
-                    }
-                }),
-            }
+  /**
+   * Filters and transforms the input data.
+   *
+   * @param {Array} data - The input data to be filtered and transformed.
+   * @returns {Array}- An array of filtered and transformed data objects.
+   */
+  static filterData (data) {
+    const filteredData = data.map(item => {
+      return {
+        region: item.region,
+        prices: item.data.map(entry => {
+          return {
+            time_start: entry.time_start,
+            time_end: entry.time_end,
+            SEK_per_kWh: entry.SEK_per_kWh,
+            EUR_per_kWh: entry.EUR_per_kWh
+          }
         })
-
-        return filteredData
-    }
-    }
+      }
+    })
+    return filteredData
+  }
+}
