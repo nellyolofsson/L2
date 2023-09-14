aed719c3adcf626221adc50d14c6b5c359834ee8
Nelly <no222hh@student.lnu.se>
2023-09-14 10:46:59 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
-import { Search } from './search.js'
+import { View } from './view.js'
+import fs from 'fs'
+import { Svgview } from './svgview.js'
 
-const api = new Search()
+const view = new View()
+const regionNames = {
+  0: 'Norra Sverige',
+  1: 'Norra mellan Sverige',
+  2: 'Södra mellan Sverige',
+  3: 'Södra Svergie'
+}
 
-console.log(api.getTodayPrice())
+/**
+ *
+ */
+async function generateAndWriteSVG () {
+  const svg = new Svgview()
+  fs.writeFileSync('out.html', await svg.getImageDiagramString())
+}
+
+generateAndWriteSVG()
+
+updateHistoricalData()
+/**
+ *
+ */
+async function updateHistoricalData () {
+  try {
+    const priceStatisticsHistorical = await view.generateHistoricalData()
+    for (const regionCode in priceStatisticsHistorical) {
+      const stats = priceStatisticsHistorical[regionCode]
+      const regionName = regionNames[regionCode] || 'Unknown Region'
+      console.log('Historicaldata:')
+      console.log(`Region: ${regionCode} (${regionName})`)
+      console.log(`Average Price: ${stats.averagePrice}`)
+      console.log(`Min Price: ${stats.minPrice}`)
+      console.log(`Max Price: ${stats.maxPrice}`)
+    }
+  } catch (error) {
+    console.error('Error fetching or displaying data:', error)
+  }
+}
